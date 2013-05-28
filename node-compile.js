233391b1515d89bcc651f2ace44283ffcf1caf8d
Morgan Engel <mrpoptart@gmail.com>
2013-05-27 19:21:56 -0700

    Added obfuscation to compile. Removed tmp file from git.

--- node-compile.js
+++ node-compile.js
@@ -1,21 +1,47 @@
+var fs = require("fs");
 var compressor = require('node-minify');
+var source = [];
 
 function minify(type)
 {
-    new compressor.minify({
-        type: type,
-        fileIn: 'resume.js',
-        fileOut: 'resume-min-gcc.js',
-        callback: function(err){
-            if(err)
+    fs.readFile('resume.js', "utf8", function(err, data){
+        var data = data.replace(/".+?"/g, function(str){
+            str=str.slice(1,-1);
+            var strArray = str.split("");
+            for(var l=0; l<strArray.length; l++)
             {
-                console.log("Error with "+this.type+": "+err);
+                var ltr = strArray[l];
+                if(ltr=="\\")
+                {
+                    ltr += strArray.splice(l+1,1);
+                }
+                if(source.indexOf(ltr)==-1)
+                {
+                    source.push(ltr);
+                }
+                strArray[l] = source.indexOf(ltr).toString(36);
             }
-            else
-            {
-                console.log(this.type + " successfully compiled.");
-            }
-        }
+            return "decode('"+strArray.join("")+"')";
+        });
+        data=data.replace("replaceableSource", "\""+source.join('')+"\"");
+        console.log(data);
+        fs.writeFile('resume-tmp.js', data, function(err){
+            new compressor.minify({
+                type: type,
+                fileIn: 'resume-tmp.js',
+                fileOut: 'resume-min-'+type+'.js',
+                callback: function(err){
+                    if(err)
+                    {
+                        console.log("Error with "+this.type+": "+err);
+                    }
+                    else
+                    {
+                        console.log(this.type + " successfully compiled.");
+                    }
+                }
+            });
+        });
     });
 }
 
--- resume.js
+++ resume.js
@@ -1,4 +1,19 @@
 (function(){
+    " !#";
+    function decode(str)
+    {
+        var strArray = str.split("");
+        for(var i = 0;  i<strArray.length; i++)
+        {
+            var ltr = strArray[i];
+            if(ltr == '1' || ltr == '2')
+            {
+                ltr += strArray.splice(i+1,1);
+            }
+            strArray[i] = replaceableSource.charAt(parseInt(ltr,36));
+        }
+        return strArray.join('');
+    }
     String.prototype.center = function(symbol)
     {
         symbol=symbol||" ";
@@ -79,7 +94,7 @@
         +paragraph("Why Everyone Else Will Want to Work with Me:","Being part of a company is more than just being an engineer. Your company started from a small idea with amazing people and now you're growing, but cautiously--culture means a lot to your team. I go out of my way to make the place I work an even more awesome place. I've sponsored Movember drives, cupcake baking competitions, guacamole cookoffs, pot lucks, push-up clubs, and I wrote software to organize runs to In-N-Out long before Zip-Car did it. I realize that corporate culture is only as good as the place you make it, and I want to make it awesome.")
         +paragraph("What I'm Looking For:","A company with a culture, people who care, software that needs help to get better, but has a purpose now. I want to contribute to something larger than myself, both at my company and with open-source projects. I want to keep growing with support from smart people, and I want to come to work every day with a smile on my face. I want to stay late because I believe in doing good work and because I really got into the groove at 4 PM. All of the other usual things apply here, too, like healthcare, a decent business plan, vision for the future and all that, but first thing's first.")
         +header("Work History")
-        +job("Say Media/VideoEgg", "Oct 2009","Present","Lead Engineer","Pioneered the creation and maintenance of many internal tools, boosting user productivity 300%. Produced extensible ad framework in Actionscript allowing for higher-dollar sales. Lead a team of four and consistently delivered products on schedule while innovating new ideas constantly. Developed, documented, and evangelized best practices where none existed.")
+        +job("Say Media/VideoEgg", "Oct 2009","Present","Lead Engineer","Pioneered the creation and maintenance of many internal tools, boosting user productivity 300%. Produced extensible ad framework in Actionscript allowing for higher-dollar sales. Lead a team of four and consistently delivered products on schedule while innovating new ideas constantly. Developed, documented, and evangelized best practices where none existed. Pioneered the adoption of HTML5 and provided best practice recommendations both internally and for customers.")
         +job("EVEO, Inc.", "Feb 2008","Oct 2009","Senior Developer","Aided in the conceptualization and execution of all facets of Flash and Flex development. Aided as many as 8 people working on diverse projects. Developed good practice and process improvements for a growing company. Met deadlines and consistently produced accurate estimates.")
         +job("WordsWorth Desktop Publishing", "Jan 2002","Dec 2008","Founder/Owner","Oversaw and aided in the creation of new websites, newsletters, brochures, graphics, manuals, and more. Managed up to five people at once to produce consistent, well-organized data for websites and manuals.")
         +job("The Pickleball Store", "Mar 2006","Dec 2011","Founder","Conceived, sourced funding for, and built an online store in a niche market. Monitored stock levels, profit margins, and budgets while increasing sales 200% per year until sold in 2011.")
